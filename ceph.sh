6c5c61b7271df42fc3b011d2b2da9182ffe08259
ygjfzhll <zhangjianfeng85@163.com>
2021-01-27 13:13:05 +0800

    update

--- ceph.sh
+++ ceph.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 rbd map chinapex/chinapexrbd
 mount /dev/rbd0 /data
+mount -t ceph 192.168.234.133:6789,192.168.234.134:6789,192.168.234.135:6789:/ /cephfs/ -o name=admin,secret=AQALog9g8suNBxAA8S57o7Rs7N/GQPi7F6MQ3w==
